4a844fe804801a86b8ec41b729d6bde0b193f5b2
juliopull <juliokilo14@gmail.com>
2024-03-31 22:26:28 -0300

    JulioReactJs

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Footer from './Footer.jsx'
+function App() {
+
+let name= 'julio'
+  return( 
+        <div><h1>pulseador supremo {name}</h1></div>
+  )
+}
+</Footer>
+export default App
---
+++ src/Footer.jsx
@@ -0,0 +1,6 @@
+
+const Footer = () => {
+  return <h2>Trabajariamos el footer</h2>;
+};
+
+export default Footer
